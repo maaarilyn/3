3bbd2347d1d59cf0ec64de8d225a93c82fb53e0a
maaarilyn <maaarilyn2828@gmail.com>
2023-08-27 10:25:33 +0000

    [Finish] 3-6

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -24,11 +24,11 @@ class ListsController < ApplicationController
   def update
     list = List.find(params[:id])
     list.update(list_params)
-    redirect_to list_path(list:id)
+    redirect_to list_path(list.id)
   end
   
   private
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
